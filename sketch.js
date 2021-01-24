b2793ea571383141712fec38f516ad0037f1a639
Elettra-P <71377175+Elettra-P@users.noreply.github.com>
2021-01-24 22:52:02 +0100

    new assignment

--- sketch.js
+++ sketch.js
@@ -1,48 +1,56 @@
-function preload() {
+let x, y;
+let w_1, w_2, h_1, h_2;
+let x_bounce = 1;
+let y_bounce = 1;
+let varLarp = 0;
+let larp_direction = 1;
 
-}
 
-var varLarp;
+
 
 function setup() {
-  varLarp = 0;
+  createCanvas(windowWidth, windowHeight);
+  background(162, 217, 206);
 
+  x = width / 2;
+  y = height / 2;
+  w_1 = 3* width / 7;
+  w_2 =  4 * width / 7;
+  h_1 = 2*height / 5;
+  h_2 = 3 * height / 5;
 }
 
 
 function draw() {
-  var ellipseWidth = 60;
 
-  createCanvas(windowWidth, windowHeight);
-  background(lerpColor(color(62, 90, 118), color(162, 217, 206), varLarp));
-  noStroke();
-  varLarp += 0.003;
-  if (varLarp > 1) {
-    varLarp = 0
+  if (frameCount%500==0){
+  w_1-=width / 14;
+  w_2+=width / 14;
+  h_1-=height / 10;
+  h_2+=height / 10;}
+  let a = frameCount / 100;
+
+  x = x + a * x_bounce;
+  y = y + a * y_bounce;
+
+  if (x < w_1 || x > w_2) {
+    x_bounce *= -1;
   }
-  fill(lerpColor(color(162, 217, 206), color(62, 90, 118), varLarp));
+  if (y <h_1 || y > h_2) {
 
-
-  var w = windowWidth;
-  var h = windowHeight;
-  var i;
-  var j;
-
-  for (j = 0; j < h; j += 40) {
-
-
-    for (i = 0; i < w; i += 40) {
-
-
-      ellipseWidth = abs(mouseX - i);
-
-      if (ellipseWidth > 60) {
-        ellipseWidth = 60
-      }
-
-       ellipse(i, j, ellipseWidth);
-    }
+    y_bounce *= -1;
   }
 
 
+  varLarp = varLarp + larp_direction * 0.003;
+  if (varLarp > 1 || varLarp<0) {
+    larp_direction *= -1;
+  }
+
+  push();
+  stroke(lerpColor(color(162, 217, 206), color(62, 90, 118), varLarp))
+  fill(lerpColor(color(62, 90, 118), color(162, 217, 206), varLarp));
+  ellipseMode(CENTER);
+  ellipse(x, y, a * 10);
+  pop();
 }
